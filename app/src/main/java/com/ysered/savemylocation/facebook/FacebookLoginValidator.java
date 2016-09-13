9de81e1b126197298f6efb9ae3932a81936ec935
Yurii Serediuk <yserediuk@n-ix.com>
2016-09-13 12:59:14 +0300

    Rename login validator interface.

--- app/src/main/java/com/ysered/savemylocation/facebook/FacebookLoginValidator.java
+++
@@ -1,7 +0,0 @@
-package com.ysered.savemylocation.facebook;
-
-public interface FacebookLoginValidator {
-
-    boolean validate(String email, String password);
-
-}
---
+++ app/src/main/java/com/ysered/savemylocation/facebook/LoginValidator.java
@@ -0,0 +1,7 @@
+package com.ysered.savemylocation.facebook;
+
+public interface LoginValidator {
+
+    boolean validate(String login, String password);
+
+}
